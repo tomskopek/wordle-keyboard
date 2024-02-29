26e8254a3ab557e00bec6c0efc40b1b20ca61595
tomskopek <tomskopek@gmail.com>
2024-02-29 09:07:56 -0500

    test with more css than js

--- index.js
+++ index.js
@@ -1,27 +1,6 @@
-function calculateKeyboardKeyWidth() {
-    const keyboardRowWidth = document.querySelector(".keyboard-row").offsetWidth;
-    const maxKeysInRow = 10;
-    const keyMargin = 6;
-    const rowMargin = 8;
-    return (keyboardRowWidth - (rowMargin * 2) - keyMargin * (maxKeysInRow - 1)) / maxKeysInRow;
-}
-
-function setKeyboardKeyWidth(width) {
-    const keys = document.querySelectorAll(".keyboard-button");
-    keys.forEach(key => {
-        key.style.width = `${width}px`;
-    });
-
-    // del and enter button can take up 1 extra key between the two of them since that row has 9 keys total
-    const deleteButton = document.getElementById("backspace");
-    deleteButton.style.width = `${width * 1.5}px`;
-    const enterButton = document.getElementById("enter");
-    enterButton.style.width = `${width * 1.5}px`;
-}
-
 function setGameDimensions() {
-    const keyboardKeyWidth = calculateKeyboardKeyWidth();
-    setKeyboardKeyWidth(keyboardKeyWidth);
+    const keyboardRowWidth = document.querySelector('.keyboard-row').offsetWidth;
+    document.documentElement.style.setProperty('--keyboard-row-width', `${keyboardRowWidth}px`);
 }
 
 window.onresize = function() {
